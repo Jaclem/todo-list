29f7f8aaed2577e7bccdd5b9c82fe19242b0fd3e
Joshua Clement <jaclem12@gmail.com>
2023-01-10 17:57:51 -0500

    Added functionality to add new task button

--- app/index.js
+++ app/index.js
@@ -7,7 +7,7 @@ const app = (() => {
 
   const projectName = document.getElementById('project-name');
   const date = document.getElementById('date');
-  const task = document.getElementById('task');
+  const taskBtn = document.querySelector('.task-btn');
 
   addBtn.addEventListener('click', () => {
     modal.classList.remove('visibility');
@@ -21,9 +21,64 @@ const app = (() => {
     e.preventDefault();
     let test1 = new Project(projectName.value, date.value, task.value);
     console.log(test1);
-  })
+  });
+
+  taskBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    addTask();
+  });
+
+  return {
+    taskBtn
+  }
 
 })();
+
+// function that creates a container, input, and button when add new task is clicked
+function addTask() {
+  const taskContainer = document.createElement('div');
+  const taskLabel = document.createElement('label');
+  const taskInput = document.createElement('input');
+  const addTask = document.createElement('button');
+  const ul = document.getElementById('task-list');
+
+  taskContainer.id = 'task-container';
+  taskLabel.textContent = 'New Task';
+  addTask.textContent = 'Add';
+  taskInput.id = 'task-input';
+
+  taskContainer.classList.add('task-container');
+
+  taskContainer.append(taskLabel, taskInput, addTask);
+  app.taskBtn.after(taskContainer);
+
+  // listens to add button click and appends a list item to it
+  addTask.addEventListener('click', (e) => {
+    const listItem = document.createElement('li');
+
+    e.preventDefault();
+
+    listItem.className = 'checkbox';
+    listItem.textContent = taskInput.value;
+    
+    ul.append(listItem);
+    taskContainer.remove();
+  });
+
+  // return {
+  //   taskContainer
+  // }
+}
+
+// const factory = (taskItems) => {
+//   const ul = document.createElement('ul');
+//   const listItem = document.createElement('li');
+
+//   taskItems = taskItems;
+
+//   addTask.taskcontainer.append(ul);
+
+// }
 
 class Project {
   constructor(name, date, task) {
